f60ca6375572e9906df9c3b0460ae7211f37820d
kevinelong <kevinelong@gmail.com>
2024-11-12 08:43:09 -0800

    first commit

---
+++ problem.js
@@ -0,0 +1,13 @@
+
+function Hello(){
+    console.log("Greetings!!!");
+}
+
+hello();
+
+let x = 10;
+
+y = X * 10;
+
+
+//Case-Sensitive
---
+++ quotes.js
@@ -0,0 +1,13 @@
+//ESCAPE(backslash) ONLY WHAT IS ON THE OUTSIDE
+// I pronounce \ as "literal" when reading it in my head.
+
+text = "So and so once said \"I ate too much\".";
+
+text2 = 'Another fellow said "I can\'t even...';
+
+modern = `A robot said "I won't do that."`; // NO ESCAPING!!!...
+
+//...unless there is a backtick inside
+backticks = `I like to use the backtick "\`" character.`; 
+
+d = new Date("1/1/2001");
