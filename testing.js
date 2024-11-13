50b0d4aa9b944eb1e0a7f9320cee4352b2667618
kevinelong <kevinelong@gmail.com>
2024-11-13 11:43:55 -0800

    tests

--- testing.js
+++ testing.js
@@ -4,27 +4,30 @@
 // Unit Testing - Testing a Single Unit (e.g. a function)
 // Not Black Box testing which which tests the whole application form the OUTSIDE
 
-function testMe(a, b){
+function testMe(a, b) {
     return a + b;
 }
 
 // console.log(testMe(6,7)) //expect 13
 testData = [
-    {inputs:[6,7], output:13},
-    {inputs:[6,7], output:99}, //bad test won't pass
-    {inputs:[12,13], output:25}
+    { inputs: [6, 7], output: 13 },
+    { inputs: [6, 7], output: 99 }, //bad test won't pass
+    { inputs: [12, 13], output: 25 }
 ];
 
-function runner(fn, ioList){
-    ioList.forEach((test,i) => {
+// specific one-liner without runner function
+testData.forEach((t, i) => console.log(i, testMe(...t.inputs) === t.output ? "PASS" : "FAIL"));
+// generic one liner definition of generic runner function
+const runner1 = (fn,ioList)=> ioList.forEach((t, i) => console.log(i, fn(...t.inputs) === t.output ? "PASS" : "FAIL"));
+runner1(testMe, testData); // use runner1
+
+//long form generic test runner
+function runner(fn, ioList) {
+    ioList.forEach((test, i) => {
         result = fn(...test.inputs); //... spread , unpacks into individual (not an array) comma separated vaiables
         success = result === test.output; //match expected?
-        if(success){
-            console.log("PASS",i)
-        }else{
-            console.log("FAIL",i)
-        }
+        console.log(i, success ? "PASS" : "FAIL");
     });
 }
-
+//use it
 runner(testMe, testData);
