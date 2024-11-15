aee9fdeefb050d29d8c629b9bd208d935f9e3d3e
kevinelong <kevinelong@gmail.com>
2024-11-15 07:29:15 -0800

    folder links 1

---
+++ bookmarks/data.js
@@ -0,0 +1,13 @@
+
+const folders = [ //categories?
+    "HTML", // index == 0
+    "CSS", // index == 1
+    "JavaScript",// index == 2
+    "Other Design Resources",
+    "..."
+]; //end folders
+
+const links = [
+    { folder: 0, display_name: "MDN", desc: "Mozilla...", href: "https://developer.mozilla.org" },
+    { folder: 0, display_name: "W3S", desc: "W3 School", href: "https://www.w3schools.com/" },
+];// end links
